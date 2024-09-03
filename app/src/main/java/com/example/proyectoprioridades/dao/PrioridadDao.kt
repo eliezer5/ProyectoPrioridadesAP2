bd335864781b652d49733e14822b675179264162
Eliezer Terrero Rojas <eliezer_terrero@ucne.edu.do>
2024-09-02 21:30:40 -0400

    creando PrioridadDao

---
+++ app/src/main/java/com/example/proyectoprioridades/dao/PrioridadDao.kt
@@ -0,0 +1,32 @@
+package com.example.proyectoprioridades.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Upsert
+import com.example.proyectoprioridades.entities.PrioridadEntity
+import java.util.concurrent.Flow
+
+@Dao
+interface PrioridadDao {
+    @Upsert
+    suspend fun save(prioridad: PrioridadEntity)
+
+    @Query(
+        """
+        SELECT * 
+        FROM Prioridades 
+        WHERE prioridadId=:id 
+        LIMIT 1
+        """
+    )
+    suspend fun find(id: Int):PrioridadEntity?
+
+
+    @Delete
+    suspend fun delete(prioridad: PrioridadEntity)
+
+    @Query("Select * From prioridades")
+    fun getAll(): kotlinx.coroutines.flow.Flow<List<PrioridadEntity>>
+}
