d00eccb307b869423b5d21187f98009c73a66182
Eliezer Terrero Rojas <eliezer_terrero@ucne.edu.do>
2024-09-04 12:50:47 -0400

    haciendo que el teclado aparezca numerico para el input 'Dias compromiso'

--- app/src/main/java/com/example/proyectoprioridades/MainActivity.kt
+++ app/src/main/java/com/example/proyectoprioridades/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
@@ -37,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
@@ -106,7 +108,8 @@ class MainActivity : ComponentActivity() {
                             label = { Text(text = "Dias Compromiso")},
                             value = diaCompromiso,
                             onValueChange = {diaCompromiso = it},
-                            modifier = Modifier.fillMaxWidth()
+                            modifier = Modifier.fillMaxWidth(),
+                            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
                         )
 
                         Spacer(modifier = Modifier.padding(2.dp))
